b4492b6a7012c0789fb04037687158d6da795696
Steve <stevematdavies@gmail.com>
2016-10-12 13:53:09 +0300

    refactoring

--- main.go
+++ main.go
@@ -12,49 +12,54 @@ import (
 	"xi2.org/x/xz"
 )
 
+// Root ..
 type Root struct {
 	XMLName xml.Name  `xml:"OMeS"`
 	PMSetup []PMSetup `xml:"PMSetup"`
 }
 
+// PMSetup ...
 type PMSetup struct {
-	XMLName    xml.Name   `xml:"PMSetup"`
-	Interval   string     `xml:"interval,attr"`
-	StartTime  string     `xml:"startTime,attr"`
-	PMMOResult PMMOResult `xml:"PMMOResult"`
+	XMLName                 xml.Name                `xml:"PMSetup"`
+	Interval                string                  `xml:"interval,attr"`
+	StartTime               string                  `xml:"startTime,attr"`
+	MeasurementOutputResult MeasurementOutputResult `xml:"PMMOResult"`
 }
 
-type PMMOResult struct {
-	XMLName xml.Name `xml:"PMMOResult"`
-	MO      MO
-	NEWBTS  NEWBTS `xml:"NE-WBTS_1.0"`
+// MeasurementOutputResult ...
+type MeasurementOutputResult struct {
+	XMLName           xml.Name `xml:"PMMOResult"`
+	MeasurementOutput MeasurementOutput
+	MeasurementList   MeasurementList `xml:"NE-WBTS_1.0"`
 }
 
-type NEWBTS struct {
-	XMLName         string          `xml:"NE-WBTS_1.0"`
-	MeasurementType string          `xml:"measurementType,attr"`
-	Counters        []DeviceCounter `xml:",any"`
-}
-
-type DeviceCounter struct {
-	XMLName xml.Name
-	Content string `xml:",innerxml"`
-}
-
-type MO struct {
+// MeasurementOutput ...
+type MeasurementOutput struct {
 	XMLName   xml.Name `xml:"MO"`
 	BaseID    string   `xml:"baseId"`
 	LocalMoID string   `xml:"localMoid"`
 }
 
+// MeasurementList ...
+type MeasurementList struct {
+	XMLName      string        `xml:"NE-WBTS_1.0"`
+	Type         string        `xml:"measurementType,attr"`
+	Measurements []Measurement `xml:",any"`
+}
+
+// Measurement ...
+type Measurement struct {
+	XMLName xml.Name
+	Content string `xml:",innerxml"`
+}
+
+// Counter - Represents an entire Counter Object
 type Counter struct {
 	StartTime       string
 	BaseID          string
 	MeasurementType string
-	WbitCounts      map[string]string
+	Measurements    map[string]string
 }
-
-/* ************************************************************************************************ */
 
 func check(err error) {
 	if err != nil {
@@ -65,39 +70,39 @@ func check(err error) {
 func main() {
 	data := getCountersData()
 	fmt.Println(data)
+	// TODO send data!
 }
 
-func conStruct(r Root) []Counter {
+func conStruct(rootElement Root) []Counter {
 
-	pmSetups := r.PMSetup
+	pmSetupArray := rootElement.PMSetup
+	var countersList []Counter
 
-	var ctrscol []Counter
+	for i := 0; i < len(pmSetupArray); i++ {
+		counter := Counter{}
+		pmSetup := pmSetupArray[i]
+		measurements := pmSetup.MeasurementOutputResult.MeasurementList.Measurements
+		measurementsMap := make(map[string]string)
+		counter.StartTime = pmSetup.StartTime
+		counter.BaseID = pmSetup.MeasurementOutputResult.MeasurementOutput.BaseID
+		counter.MeasurementType = pmSetup.MeasurementOutputResult.MeasurementList.Type
 
-	for i := 0; i < len(pmSetups); i++ {
-		ctr := Counter{}
-		ps := pmSetups[i]
-		ctrArr := ps.PMMOResult.NEWBTS.Counters
-		ctrMap := make(map[string]string)
-		ctr.StartTime = ps.StartTime
-		ctr.BaseID = ps.PMMOResult.MO.BaseID
-		ctr.MeasurementType = ps.PMMOResult.NEWBTS.MeasurementType
-
-		for j := 0; j < len(ctrArr); j++ {
-			dctr := ctrArr[j]
-			dcName := dctr.XMLName.Local
-			dcContent := dctr.Content
-			ctrMap[dcName] = dcContent
+		for j := 0; j < len(measurements); j++ {
+			measure := measurements[j]
+			name := measure.XMLName.Local
+			content := measure.Content
+			measurementsMap[name] = content
 		}
 
-		ctr.WbitCounts = ctrMap
-		ctrscol = append(ctrscol, ctr)
+		counter.Measurements = measurementsMap
+		countersList = append(countersList, counter)
 	}
-	return ctrscol
+	return countersList
 }
 
-func xmlify(f []byte) []Counter {
+func xmlify(byts []byte) []Counter {
 	root := Root{}
-	err := xml.Unmarshal(f, &root)
+	err := xml.Unmarshal(byts, &root)
 	check(err)
 	return conStruct(root)
 }
@@ -109,20 +114,20 @@ func getCountersData() []string {
 }
 
 func path() string {
-	pmFile := "/tmp/stats/pm.counter.xml"
-	m, err := filepath.Glob("/tmp/stats/*.raw.xz")
+	pmXMLFilePath := "/tmp/stats/pm.counter.xml"
+	matches, err := filepath.Glob("/tmp/stats/*.raw.xz")
 	check(err)
-	if len(m) > 0 {
-		data, err := ioutil.ReadFile(m[0])
+	if len(matches) > 0 {
+		data, err := ioutil.ReadFile(matches[0])
 		check(err)
-		rdr, err := xz.NewReader(bytes.NewReader(data), 0)
+		readable, err := xz.NewReader(bytes.NewReader(data), 0)
 		check(err)
-		var outBfr bytes.Buffer
-		_, err = outBfr.ReadFrom(rdr)
+		var byts bytes.Buffer
+		_, err = byts.ReadFrom(readable)
 		check(err)
-		err = ioutil.WriteFile(pmFile, outBfr.Bytes(), 0644)
+		err = ioutil.WriteFile(pmXMLFilePath, byts.Bytes(), 0644)
 		check(err)
-		return pmFile
+		return pmXMLFilePath
 	}
 	return ""
 }
@@ -130,8 +135,8 @@ func path() string {
 func jsonFromXML(countersArray []Counter) []string {
 	var JSONCounterArray []string
 	for i := 0; i < len(countersArray); i++ {
-		c := countersArray[i]
-		jsn, err := json.Marshal(c)
+		counter := countersArray[i]
+		jsn, err := json.Marshal(counter)
 		check(err)
 		JSONCounterArray = append(JSONCounterArray, string(jsn)+",")
 	}
@@ -139,7 +144,7 @@ func jsonFromXML(countersArray []Counter) []string {
 }
 
 func stripArrayTrailingComma(arr []string) []string {
-	i := len(arr) - 1
-	arr[i] = strings.TrimSuffix(arr[i], ",")
+	index := len(arr) - 1
+	arr[index] = strings.TrimSuffix(arr[index], ",")
 	return arr
 }
